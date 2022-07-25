0a1c92c382d73c41eb7e3c4453fd3eab4b4c7cc9
sreelekha-2 <sreelekhakutikanti@gmail.com>
2022-07-25 18:34:20 +0530

    1 file modified, 1 added

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,15 @@
+// import { Component } from 'react';
+
+// class ErrorBoundary extends Component {
+//   getDerivedStateFromError() {
+//     console.log('get derived fired');
+//   }
+//   componentDidCatch(error, info) {
+//     console.log('componennet fired');
+//   }
+//   render() {
+//     return <div>Error Occured</div>;
+//   }
+// }
+
+// export default ErrorBoundary;
